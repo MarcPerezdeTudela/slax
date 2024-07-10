7619a4e01fee58fe7bd3831d72c02ef2df9b93ec
Marc Perez de Tudela <mperezdetudelaaznar@gmail.com>
2024-07-10 21:36:33 +0200

    extract Chat context

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,11 +1,11 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
 
-  alias Slax.Repo
+  alias Slax.Chat
   alias Slax.Chat.Room
 
   def mount(_params, _session, socket) do
-    rooms = Repo.all(Room)
+    rooms = Chat.list_rooms()
     {:ok, assign(socket, rooms: rooms)}
   end
 
@@ -17,10 +17,10 @@ defmodule SlaxWeb.ChatRoomLive do
     room =
       case Map.fetch(params, "id") do
         {:ok, id} ->
-          Repo.get!(Room, id)
+          Chat.get_room!(id)
 
         :error ->
-          List.first(socket.assigns.rooms)
+          Chat.get_first_room!()
       end
 
     {:noreply, assign(socket, hide_topic?: false, room: room)}
